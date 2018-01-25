69f7a547b2a6241263827ec27b7b15abc3ba8e1f
Lucas Weed <Lucas Weed@LAPTOP-I12V6R64>
2018-01-25 14:33:41 -0600

    there was a disaster

--- inclass_assignment2_lucas_weed.m
+++ inclass_assignment2_lucas_weed.m
@@ -20,12 +20,6 @@ temp_fridge_flux = temp_fridge*(percent_flux)*(0.01); % Degrees in Fahrenheit th
 temp_fridge_max = temp_fridge + temp_fridge_flux; % Max temperature the fridge can reach
 temp_fridge_min = temp_fridge - temp_fridge_flux; % Minimum temperature the fridge can drop to
 
-<<<<<<< HEAD
-%temp_fridge_all = temp_fridge_min: 1: temp_fridge_max % Whole number temperatures inside the fridge range
-%still buggy
-%log((temp_desired - temp_fridge_all)*((temp_current - temp_fridge_all).^-1))*(-k.^-1)
-fprintf('Dude it''s going to take %g hours to chill the drinks, the party is going to be over by then!', time_to_cool) % Prints results as a string
-=======
 temp_fridge_range = [temp_fridge_min, temp_fridge, temp_fridge_max] % Array for the temp range of the fridge
 
 dt_desired_flux = temp_desired - temp_fridge_range; % Temp difference between desired temp and fridge with flux
@@ -33,4 +27,3 @@ dt_current_flux = temp_current - temp_fridge_range; % Temp difference between cu
 time_to_cool_range = (log(dt_desired_flux./ dt_current_flux))/-k % Newton's cooling equation solved for t, eval. with flux
 
 fprintf('Dude it''s going to take %2.1f hours to chill the drinks, the party is going to be over by then!', time_to_cool);
->>>>>>> ebdd82ef072072fa15dadef810fc2885e6904e4f
