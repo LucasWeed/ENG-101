08ee7ff3c1ec66c5d15d224737c39ad255f21ce4
Lucas Weed <Lucas Weed@DESKTOP-7U230Q9>
2018-01-24 23:40:33 -0600

    -Added array based temperature fluctuation, it's being a problem, commented out -Added a print function to return time to cool in a sentence

--- inclass_assignment2_lucas_weed.m
+++ inclass_assignment2_lucas_weed.m
@@ -11,4 +11,14 @@ temp_fridge = input('In Fahrenheit, what temperature is your fridge? ');    %Inp
 temp_desired = input('In Fahrenheit, what temperature do you want your beer? '); %Input for the desired temperature Tc
 k = 0.35; %energy constant
 
-time_to_cool = (log((temp_desired-temp_fridge)/(temp_current-temp_fridge))/-k) % Solve Newton's law of cooling for t.
+time_to_cool = (log((temp_desired-temp_fridge)/(temp_current-temp_fridge))/-k); % Solve Newton's law of cooling for t.
+
+%percent_flux = 20 % Percentage that fridge temperature can fluxtuate
+%temp_fridge_flux = temp_fridge*(percent_flux)*(0.01) % Degrees in Fahrenheit that fridge can fluctuate to
+%temp_fridge_max = temp_fridge + temp_fridge_flux % Max temperature the fridge can reach
+%temp_fridge_min = temp_fridge - temp_fridge_flux % Minimum temperature the fridge can drop to
+
+%temp_fridge_all = temp_fridge_min: 1: temp_fridge_max % Whole number temperatures inside the fridge range
+
+%log((temp_desired - temp_fridge_all)*((temp_current - temp_fridge_all).^-1))*(-k.^-1)
+fprintf('Dude it''s going to take %g hours to chill the drinks, the party is going to be over by then!', time_to_cool)
